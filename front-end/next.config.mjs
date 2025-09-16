3f1d3299a9768296c6f2319f6217fe0975da4afc
Ethan5574 <tranvanhaubg2003@gmail.com>
2025-09-16 15:02:42 +0700

    fix: next.config

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,11 +1,25 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
+import envsConfig from './envs.js';
+
+const envs = envsConfig[process.env.MODE];
+if (envs) {
+  console.log(`MODE: ${process.env.MODE}`);
+  Object.keys(envs).forEach((key) => {
+    process.env[key] = envs[key];
+  });
+  console.log(`Website Url: ${process.env.NEXT_PUBLIC_FE_URL}`);
+}
+
+const nextConfig = {
   images: {
-    unoptimized: true
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
